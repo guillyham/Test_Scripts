3910507c2659424ba631df15528350897b032161
G T <guillyham@gmail.com>
2025-08-13 07:56:33 -0300

    Mudanças minimas

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,15 +12,15 @@ export default defineConfig({
   use: {
     browserName: 'chromium',
     ...devices['Desktop Chrome'],
-    trace: 'on',
-    screenshot: 'on',
-    video: 'retain-on-failure',
+    //trace: 'on',
+    //screenshot: 'on',
+    //video: 'retain-on-failure',
   },
 
   projects: [
     //teste da base: teste final
     { name: '01-Ajax-Estoque',  testMatch: ['**/TesteFinal/Ajax/validacaoAjaxEstoque.spec.ts'] },
-    { name: '02-Ajax-Pedidos',  testMatch: ['**/TesteFinal/Ajax/validacaoAjaxPedidos.spec.ts'], dependencies: ['01-Ajax-Estoque'] },
+    { name: '02-Ajax-Pedidos',  testMatch: ['**/TesteFinal/Ajax/validacaoAjaxPedidos.spec.ts'], dependencies: ['01-Ajax-Estoque']   },
     { name: '03-Cad-Cliente',   testMatch: ['**/Cad_cliente.spec.ts'], dependencies: ['02-Ajax-Pedidos'], use: { headless: false, launchOptions: { slowMo: 200 } } },
     { name: '04-Cad-Planos',    testMatch: ['**/Cad_Planos.spec.ts'], dependencies: ['03-Cad-Cliente'] },
 
